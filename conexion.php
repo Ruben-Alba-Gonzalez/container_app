0ea526c9facaa953e58f7a7b958260af637ebca8
Ruben <ruben.algo99@gmail.com>
2021-12-10 09:24:30 +0100

    prueba credenciales

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 $nombre = $_POST['nombre'];
 $pass = $_POST['password'];
 
-$con = mysqli_connect("database:3306","ruben","ruben","container_database");
+$con = mysqli_connect("database","ruben","ruben","container_database");
 $query = "select * from clientes where username='".$nombre."' and contra='".$pass."'";
 
 if($con){
